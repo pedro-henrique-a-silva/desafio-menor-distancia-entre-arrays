6e6f80148943887b30ab65657eeb2779c301374f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-20 17:32:52 -0300

    Função menor distancia do array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ordenador;
+
+
+public class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        int[] arrayNumeros1 = {-5, 6, 14, 32, 27, 2};
+        int[] arrayNumeros2 = {30, 40, 52, 9, 19};
+
+        ArrayFunctions.Sort(arrayNumeros1); 
+        ArrayFunctions.Sort(arrayNumeros2);
+
+        int[] resultado = ArrayFunctions.LowerDistance(arrayNumeros1, arrayNumeros2);
+
+        Console.WriteLine(string.Join(" ", arrayNumeros1));
+        Console.WriteLine(string.Join(" ", arrayNumeros2));
+        Console.WriteLine(string.Join(" ", resultado));
+    } 
+}
